d2fd0de7d9d9780b491ad4e22f0dc8b2647dcf8b
sar0868 <safarov-ar@yandex.ru>
2025-03-04 23:03:57 +0300

    in progress

--- internal/models/valute.go
+++ internal/models/valute.go
@@ -1,14 +1,20 @@
 package models
 
 type Valute struct {
-	ID      string  `json:"id"`
-	NumCode string  `json:"numCode"`
-	Nominal int     `json:"nominal"`
-	Name    string  `json:"name"`
-	Value   float64 `json:"value"`
+	ID      string
+	NumCode string
+	Nominal int
+	Name    string
+	Value   float64
+	// ID      string  `json:"id"`
+	// NumCode string  `json:"numCode"`
+	// Nominal int     `json:"nominal"`
+	// Name    string  `json:"name"`
+	// Value   float64 `json:"value"`
 }
 
 type CurrenciesData struct {
-	Date   string `json:"date"`
+	Date string
+	// Date   string `json:"date"`
 	Values map[string]Valute
 }
--- internal/service/getData.go
+++ internal/service/getData.go
@@ -1,13 +1,15 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
+
+	"github.com/sar0868/currency_converter/internal/models"
 )
 
 func GetData() (string, error) {
+	var data models.CurrenciesData
 	// url := "https://www.cbr.ru/scripts/XML_daily.asp"
 	url := "https://www.cbr-xml-daily.ru/daily_json.js"
 	resp, err := http.Get(url) //nolint: noctx
@@ -18,7 +20,11 @@ func GetData() (string, error) {
 	if resp.StatusCode != http.StatusOK {
 		return "", fmt.Errorf("error get data")
 	}
-	io.Copy(os.Stdout, resp.Body)
+	errDecode := json.NewDecoder(resp.Body).Decode(&data)
+	if errDecode != nil {
+		return "", fmt.Errorf("error decode: %w", errDecode)
+	}
+	fmt.Println(data.Values["AUD"])
 	// // body, err :=
 	return "", nil
 }
