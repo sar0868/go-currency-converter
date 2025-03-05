3c944452da038b67a9d47b57ec7048776855d5f6
sar0868 <safarov-ar@yandex.ru>
2025-03-06 00:16:31 +0300

    edit models

--- internal/models/valute.go
+++ internal/models/valute.go
@@ -1,13 +1,13 @@
 package models
 
 type Data struct {
-	NumCode  string  `xml:"NumCode" json:"numCode"`
-	CharCode string  `xml:"CharCode" json:"charCode"`
-	Nominal  int     `xml:"Nominal" json:"nominal"`
-	Name     string  `xml:"Name" json:"name"`
-	Value    float64 `xml:"Value" json:"value"`
+	NumCode  string  `json:"numCode"`
+	CharCode string  `json:"charCode"`
+	Nominal  int     `json:"nominal"`
+	Name     string  `json:"name"`
+	Value    float64 `json:"value"`
 }
 
 type CurrenciesData struct {
-	Valute map[string]Data `xml:"Valute" json:"valute"`
+	Valute map[string]Data `json:"valute"`
 }
