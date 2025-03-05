838e120e23a0d6a5b523f6572a1778ecbf577c9d
sar0868 <safarov-ar@yandex.ru>
2025-03-05 21:51:23 +0300

    add inputRequest

--- internal/service/inputRequest.go
+++ internal/service/inputRequest.go
@@ -8,28 +8,13 @@ import (
 )
 
 func Input() (models.InputData, error) {
-	// var inp string
 	var input models.InputData
-	fmt.Println(input)
 
 	fmt.Println("input count curr1 curr2")
 	var count float64
 	var exchanged string
 	var received string
-	// fmt.Scan(&inp)
-	// fmt.Println(inp)
-	// inpArr := strings.Split(inp, " ")
-	// fmt.Println(inpArr)
 
-	// if len(inpArr) != 3 {
-	// 	return input, fmt.Errorf("incorrect input data")
-	// }
-	// count, errCnt := strconv.ParseFloat(inpArr[0], 64)
-	// if errCnt != nil {
-	// 	return input, errCnt
-	// }
-	// exchanged := inpArr[1]
-	// received := inpArr[2]
 	fmt.Scan(&count)
 	fmt.Scan(&exchanged)
 	fmt.Scan(&received)
@@ -37,7 +22,7 @@ func Input() (models.InputData, error) {
 	if count <= 0 {
 		return input, fmt.Errorf("can't be less than 0: ")
 	}
-	valid, err := enum.ValidateSlice(models.ValidList, []string{exchanged, received})
+	valid, err := enum.ValidateSlice([]string{exchanged, received}, models.ValidList)
 	if err != nil {
 		return input, err
 	}
@@ -49,6 +34,5 @@ func Input() (models.InputData, error) {
 		Exchanged: models.ValName(exchanged),
 		Received:  models.ValName(received),
 	}
-	fmt.Println(input)
 	return input, nil
 }
